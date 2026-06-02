91ba8adfca7e64db62f2cf1deeac5b5297c8b589
Apleax <apleax@foxmail.com>
2026-06-02 14:59:17 +0800

    Fixed standalone mode web server not rendering  marks

--- src/main/java/cn/lunadeer/mc/dominionSAR/providers/BlueMapStandaloneMode.java
+++ src/main/java/cn/lunadeer/mc/dominionSAR/providers/BlueMapStandaloneMode.java
@@ -7,6 +7,9 @@ import cn.lunadeer.mc.dominionSAR.MapProvider;
 import cn.lunadeer.mc.dominionSAR.providers.bluemap.BlueMapMarkerSupport;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import de.bluecolored.bluemap.api.BlueMapAPI;
+import de.bluecolored.bluemap.api.gson.MarkerGson;
+import de.bluecolored.bluemap.api.markers.MarkerSet;
 import org.bukkit.World;
 
 import java.awt.Color;
@@ -104,6 +107,11 @@ public class BlueMapStandaloneMode implements MapProvider {
             Path tempFile = Files.createTempFile(markerFile.getParent(), "markers-", ".json.tmp");
             Files.writeString(tempFile, gson.toJson(markerSets), StandardCharsets.UTF_8);
             moveIntoPlace(tempFile, markerFile);
+            MarkerSet marker = MarkerGson.INSTANCE.fromJson(gson.toJsonTree(markerSets.get("Dominion")), MarkerSet.class);
+            BlueMapAPI.getInstance().flatMap(api ->
+                    api.getMap(worldName)).ifPresent(map -> {
+                map.getMarkerSets().put("Dominion", marker);
+            });
         } catch (IOException e) {
             DominionSAR.getInstance().getLogger().warning("Failed to write BlueMap standalone markers for world '" + worldName + "': " + e.getMessage());
         }
